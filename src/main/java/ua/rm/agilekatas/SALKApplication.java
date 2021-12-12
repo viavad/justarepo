52cb4f1d3b9fde81bd5aea0149937b626c1f4933
rmd00000 <thisforwork@ukr.net>
2021-12-12 17:14:42 +0200

    naked project

---
+++ src/main/java/ua/rm/agilekatas/SALKApplication.java
@@ -0,0 +1,13 @@
+package ua.rm.agilekatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SALKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SALKApplication.class, args);
+	}
+
+}
